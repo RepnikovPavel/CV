848b5896a3c5b4f9606e0e52198896c8d4d22786
Repnikov Pavel <prepnik@mail.ru>
2024-01-27 14:01:25 +0300

    1

--- build.py
+++ build.py
@@ -1,8 +1,22 @@
 import os
+import shutil
+
+def clear_folder(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
 oname = 'PavelRepnikovCV.pdf'
 currd = os.path.dirname(os.path.realpath(__file__))
 texfile = os.path.join(currd, 'main.tex')
 odir = os.path.join(currd,'build')
+clear_folder(odir)
 ofile = os.path.join(odir,'main.pdf')
 oname = os.path.join(odir,oname)
 if not os.path.exists(odir):
